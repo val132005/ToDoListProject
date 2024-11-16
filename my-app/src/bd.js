36c715941a158ac4344f0ce3255ac2824e459f49
val132005 <valentina.buenhombre13@gmail.com>
2024-11-16 01:51:06 -0500

    Se a;adio archivo de bd hasura

--- my-app/src/bd.js
+++ my-app/src/bd.js
@@ -1,6 +1,10 @@
 
 /*
 
+DROP TYPE IF EXISTS state_item;
+DROP TYPE IF EXISTS state_user;
+
+
 -- Crear tipo ENUM para el estado de los ítems
 CREATE TYPE state_item AS ENUM ('undone', 'done', 'in_process');
 
@@ -33,25 +37,26 @@ CREATE TABLE item (
 
 );
 
+CREATE TABLE users (
+    id_user SERIAL PRIMARY KEY,                 -- ID autoincrementable
+    name_user TEXT NOT NULL,                    -- Nombre del usuario
+    document_user BIGINT UNIQUE NOT NULL,       -- Documento único del usuario
+    state_user state_user NOT NULL,             -- Estado del usuario (ENUM)
+    password_user TEXT NOT NULL,                 -- Contraseña del usuario
+    id_todolist INT UNIQUE NOT NULL,
+    CONSTRAINT fk_todolist_user FOREIGN KEY (id_todolist) REFERENCES toDoList(id_todolist)
+);
+
 
 CREATE TABLE photoGallery (
     id_photogallery SERIAL PRIMARY KEY,         -- ID autoincrementable
     name_photogallery TEXT NOT NULL,            -- Nombre de la galería de fotos
     id_user INT UNIQUE NOT NULL,                       -- FK para la relación con users
     id_photo INT UNIQUE NOT NULL,                               -- FK para la relación con photos (uno a muchos)
-    CONSTRAINT fk_user_photogallery FOREIGN KEY (id_user) REFERENCES user(id_user),   -- Relación 1:1 con users
+    CONSTRAINT fk_user_photogallery FOREIGN KEY (id_user) REFERENCES users(id_user),   -- Relación 1:1 con users
     CONSTRAINT fk_photo_photogallery FOREIGN KEY (id_photo) REFERENCES photo(id_photo) -- Relación 1:N con photo
 );
 
-CREATE TABLE user (
-    id_user SERIAL PRIMARY KEY,                 -- ID autoincrementable
-    name_user TEXT NOT NULL,                    -- Nombre del usuario
-    document_user BIGINT UNIQUE NOT NULL,       -- Documento único del usuario
-    state_user state_user NOT NULL,             -- Estado del usuario (ENUM)
-    password_user TEXT NOT NULL                 -- Contraseña del usuario
-    id_todolist INT UNIQUE NOT NULL,
-    CONSTRAINT fk_todolist_user FOREIGN KEY (id_todolist) REFERENCES toDoList(id_todolist)
-);
 
 
 */
